22b02ade01fdc225ba48760340be024f31579539
agorbacheva-y <agorbacheva.y@gmail.com>
2023-10-26 21:57:07 +0200

    Co-authored-by: bezerkt1 <bezerkt1@users.noreply.github.com>

--- weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
@@ -42,42 +42,9 @@ function App() {
   console.log("data", data);
   return (
     <>
-      <Card
-        buttonText="ORDER NOW"
-        buttonTextColor="#000000"
-        buttonColor="#FCED84"
-        emoji="🍋"
-        primary="#8CFFBA"
-        secondary="#CFFFE2"
-        subtitle="This is a really tasty fruit."
-        subtitleColor="#000000"
-        title="Lemon"
-        titleColor="#166F39"
-      />
-      <Card
-        buttonText="BOOK SEAT"
-        buttonTextColor="#FFD600"
-        buttonColor="#0B0C11"
-        emoji="🚀"
-        primary="#A6CFE2"
-        secondary="#C2DCE9"
-        subtitle="Join us into the outer space!"
-        subtitleColor="#000000"
-        title="Rocket"
-        titleColor="#0B0C11"
-      />
-      <Card
-        buttonText="ADOPT ME"
-        buttonTextColor="#000000"
-        buttonColor="#FCED84"
-        emoji="🐶"
-        primary="#EEB200"
-        secondary="#FFE086"
-        subtitle="Hello, do you want to be friends?"
-        subtitleColor="#502F7E"
-        title="Doggy"
-        titleColor="#502F7E"
-      />
+      {data.map((aCard) => (
+         <Card key={aCard.title} data={aCard}/>
+      ))}
     </>
   );
 }
--- weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
@@ -1,62 +1,40 @@
 import { useState } from "react";
-import "./Card.scss";
+import "./Card.css";
 
-// Passing in and destructuring the props
-const Card = ({
-  buttonColor,
-  buttonText,
-  buttonTextColor,
-  emoji,
-  primary,
-  secondary,
-  subtitle,
-  subtitleColor,
-  title,
-  titleColor,
-}) => {
-  // this state variable takes care of what "version" of the card to render
-  const [clicked, setClicked] = useState(false);
+const Card = ({ data }) => {
+  const [isClicked, setIsClicked] = useState(false);
 
-  // If the card is clicked, render this:
-  if (clicked)
-    return (
-      <article className="card clickedCard">
-        <section className="top" style={{ background: primary }}>
-          <h1 className="title" style={{ color: titleColor }}>
-            {title}
-          </h1>
-        </section>
-        <section className="bottom" style={{ background: secondary }}>
-          <span className="emoji" role="img" aria-label="emoji">
-            {emoji}
-          </span>
-        </section>
-      </article>
-    );
+  
+/*
+document.getElementById("clicked--body").addEventListener("mouseover", () => {
+    hoverBody.style.height = "100%";
+});
 
-  // If the card isn't cliked, render this:
+document.getElementById("clicked--body").addEventListener("mouseout", () => {
+    hoverBody.style.height = "50%";
+});
+*/
+
   return (
-    <article className="card">
-      <section className="top" style={{ background: primary }}>
-        <h1 className="title" style={{ color: titleColor }}>
-          {title}
-        </h1>
-        <span className="emoji" role="img" aria-label="emoji">
-          {emoji}
-        </span>
-      </section>
-      <section className="bottom" style={{ background: secondary }}>
-        <h2 className="subTitle" style={{ color: subtitleColor }}>
-          {subtitle}
-        </h2>
-        <button
-          style={{ background: buttonColor, color: buttonTextColor }}
-          onClick={() => setClicked(!clicked)}
-        >
-          {buttonText}
-        </button>
-      </section>
-    </article>
+    <>
+      {/* If the card is clicked, render this: */}
+      {isClicked ? (
+        <div className="card clicked" style={{"backgroundColor": data.primary_color, "color": data.text_color}}>
+          <header className="card__header">{data.title}</header>
+          <section className="card__body" style={{"backgroundColor": data.secondary_color}}>{data.emoji}</section>
+        </div>
+
+      ) : (
+        // If the card isn't clicked, render this:
+        <div className="card" style={{"backgroundColor": data.primary_color, "color": data.text_color}}>
+          <header className="card__header"><div className="title">{data.title}</div> <div className="emoji">{data.emoji}</div></header>
+          <section className="card__body" style={{"backgroundColor": data.secondary_color}}>
+            <p className="card__desc">{data.description}</p>
+            <button onClick={()=> setIsClicked(true)} style={{"backgroundColor": data.button_color, "color": data.button_text_color}}>{data.button_text}</button>
+          </section>
+        </div>
+      )}
+    </>
   );
 };
 
