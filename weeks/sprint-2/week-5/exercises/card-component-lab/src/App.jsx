cc154cabace863b19af50a57a098c8f7511da180
agorbacheva-y <agorbacheva.y@gmail.com>
2023-10-26 21:57:07 +0200

    Co-authored-by: bezerkt1 <bezerkt1@users.noreply.github.com>

--- weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
@@ -1,10 +1,50 @@
 import "./App.css";
 import Card from "./components/Card";
 
+const data = [
+  {
+    title: "Lemon",
+    description: "This is a really tasty fruit",
+    emoji: "🍋",
+    text_color: "#166F39",
+    primary_color: "#8CFFBA",
+    secondary_color: "#CFFFE2",
+    button_text: "Order now",
+    button_color: "#FCED84",
+    button_text_color: "#000000"
+  },
+  {
+    title: "Rocket",
+    description: "Join us into the outer space",
+    emoji: "🚀",
+    text_color: "#0B0C11",
+    primary_color: "#A6CFE2",
+    secondary_color: "#C2DCE9",
+    button_text: "Book seat",
+    button_color: "#0B0C11",
+    button_text_color: "#FFD600"
+  }, 
+  {
+    title: "Doggy",
+    description: "Hello do you want to be friends?",
+    emoji: "🐶",
+    text_color: "#502F7E",
+    primary_color: "#EEB200",
+    secondary_color: "#FFE086",
+    button_text: "Adopt me",
+    button_color: "#FFFFFF",
+    button_text_color: "#502F7E"
+  }
+];
+
+
 function App() {
+  console.log("data", data);
   return (
     <>
-      <Card />
+      {data.map((aCard) => (
+         <Card key={aCard.title} data={aCard}/>
+      ))}
     </>
   );
 }
--- weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import "./Card.css";
 
-const Card = () => {
-  // If the card is clicked, render this:
-  // return ();
+const Card = ({ data }) => {
+  const [isClicked, setIsClicked] = useState(false);
 
-  // If the card isn't cliked, render this:
-  return <div>Card goes here</div>;
+  
+/*
+document.getElementById("clicked--body").addEventListener("mouseover", () => {
+    hoverBody.style.height = "100%";
+});
+
+document.getElementById("clicked--body").addEventListener("mouseout", () => {
+    hoverBody.style.height = "50%";
+});
+*/
+
+  return (
+    <>
+      {/* If the card is clicked, render this: */}
+      {isClicked ? (
+        <div className="card clicked" style={{"backgroundColor": data.primary_color, "color": data.text_color}}>
+          <header className="card__header">{data.title}</header>
+          <section className="card__body" style={{"backgroundColor": data.secondary_color}}>{data.emoji}</section>
+        </div>
+
+      ) : (
+        // If the card isn't clicked, render this:
+        <div className="card" style={{"backgroundColor": data.primary_color, "color": data.text_color}}>
+          <header className="card__header"><div className="title">{data.title}</div> <div className="emoji">{data.emoji}</div></header>
+          <section className="card__body" style={{"backgroundColor": data.secondary_color}}>
+            <p className="card__desc">{data.description}</p>
+            <button onClick={()=> setIsClicked(true)} style={{"backgroundColor": data.button_color, "color": data.button_text_color}}>{data.button_text}</button>
+          </section>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Card;
